11ebd134ba0fa13dabea159555904cf993112ab9
claudiotudominusculo <claudiotudominusculo@gmail.com>
2021-09-08 16:56:07 -0300

    Initial commit

---
+++ src/main/java/com/beerstock/BeerStock/repository/BeerRepository.java
@@ -0,0 +1,12 @@
+package com.beerstock.BeerStock.repository;
+
+
+import com.beerstock.BeerStock.entity.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BeerRepository extends JpaRepository<Beer, Long> {
+
+    Optional<Beer> findByName(String name);
+}
